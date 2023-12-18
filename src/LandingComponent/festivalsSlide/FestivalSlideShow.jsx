75f7cac0f67fdbb6436edb307e2022d426ba2c1b
mikofrac <m.ogbechie@native.semicolon.africa>
2023-12-18 06:28:26 +0100

    Upgraded festival section : i uses slick react to work on the slides

--- src/LandingComponent/festivalsSlide/FestivalSlideShow.jsx
+++ src/LandingComponent/festivalsSlide/FestivalSlideShow.jsx
@@ -1,27 +1,38 @@
+import React from "react";
+import Slider from "react-slick";
 import "./FestivalSlideShow.css";
-import React, { useState } from "react";
 import ImageCard from "./ImageCard";
 import img1 from "../assets/9ja_one.jpeg";
 import img2 from "../assets/9ja_six.jpeg";
 import img3 from "../assets/9ja_five.jpeg";
-function FestivalSlideShow() {
-    const [imgOne,setImgOne]= useState(img1)
-    const [imgTwo,setImgTwo]= useState(img2)
-    const [imgThree,setImgThree]= useState(img3)
-    function changeImages(){
-        
-    }
+import img5 from "../assets/9ja_three.jpeg"
+import img4 from "../assets/9ja_four.jpeg"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+const FestivalSlideShow = () => {
+  const data = [{ pic: img1 }, { pic: img2 }, { pic: img3 }, {pic: img4}, {pic: img5}];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
 
   return (
     <div className="festival-slide-show">
-      <h1 className="caption">Our Culuture Our Tradition</h1>
+      <h1 className="caption">Our Culture Our Tradition</h1>
       <div className="wrapper">
-        <ImageCard image={imgOne} />
-        <ImageCard image={imgTwo} />
-        <ImageCard image={imgThree} />
+        <Slider {...settings}>
+          {data.map((d, index) => (
+            <ImageCard key={index} image={d.pic} />
+          ))}
+        </Slider>
       </div>
     </div>
   );
-}
+};
 
 export default FestivalSlideShow;
