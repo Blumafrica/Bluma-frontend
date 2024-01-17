4e6f6be3d3ffd00167f321bebef2c1c57023fe5e
mikofrac <m.ogbechie@native.semicolon.africa>
2024-01-17 09:54:27 +0100

    sliding hero section implemented

--- src/LandingComponent/HeroSection.jsx
+++ src/LandingComponent/HeroSection.jsx
@@ -1,28 +1,112 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowForwardIos";
+import { Slideritems } from "./data";
+import SignUpBtn from "./Button&Search/SignUpBtn";
 import "./heroSection.css";
+import NavBar from "./NavBar";
 
-function HeroSection() {
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
+const Arrow = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  visibility: hidden;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+const Caption = styled.div`
+  font-size: 42px;
+  font-weight: 700;
+`;
+const TextContent = styled.div`
+  max-width: 1200px;
+  margin: 10% auto;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 2.5rem;
+  border-radius: 5px;
+
+  @media (min-width: 768px) {
+  }
+
+  @media (min-width: 1024px) {
+  }
+`;
+const Heading = styled.h1``;
+const Text = styled.p`
+  letter-spacing: 1.5px;
+  font-size: 20px;
+`;
+const HeroBtn = styled.button``;
+const Slide = styled.div`
+  color: white;
+  width: 100vw;
+  height: 100%;
+`;
+
+const HeroSection = (props) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => handleClick("left"), 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 2));
+    } else {
+      setSlideIndex((prevIndex) => (prevIndex < 2 ? prevIndex + 1 : 0));
+    }
+  };
+
   return (
-    <div className="hero-section">
-      <div className="container">
-        <div className="text-area">
-          <h1>Welcome to Nigeria where the drums sounds the loudest</h1>
-          <p>
-            Magna ensemble learning ut gpt ad in elit voluptate ipsum. Elit
-            artificial intelligence dolore AI in healthcare Facebook AI in sint
-            duis. In general intelligence action text generation mollit quis
-            drones gradient boosting data lakes esse privacy information
-            retrieval text generation ut ut ipsum et. data preprocessing elit in
-            affective computing mollit labore ea quis. Eiusmod proident
-            cupidatat nostrud aliqu ip voluptate sit NVIDIA TPUs neural
-            architecture search proident quis proident pariatur eiusmod.
-            Excepteur ELMo reprehenderit parallel computing id consectetur nulla
-          </p>
-          <button>Read more</button>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlinedIcon />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {Slideritems.map((item) => (
+          <Slide bg={item.bg} identity={item.id} className={item.class}>
+            <TextContent>
+              <Caption>Seamless</Caption>
+              <Heading>{props.header}</Heading>
+              <Text>{item.desc}</Text>
+              <div style={{display: "flex", margin: "3rem 0rem",}}>
+                <SignUpBtn>Get Started</SignUpBtn>
+                <HeroBtn>View more</HeroBtn>
+              </div>
+            </TextContent>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlinedIcon />
+      </Arrow>
+    </Container>
   );
-}
+};
 
 export default HeroSection;
--- src/LandingComponent/comingSoon/Tour.jsx
+++ src/LandingComponent/comingSoon/Tour.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
-import tourPic from "../assets/tour3.jpg"
-import "./Tour.css"
+import React, { useState } from "react";
+import tourPic from "../assets/tour3.jpg";
+import "./Tour.css";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const TextArea = styled.div`
+  flex: 1;
+`;
+const ImageArea = styled.div`
+  flex: 1;
+`;
+const Image = styled.img``;
+const List = styled.li``;
+const Wrapper = styled.div`
+  color: black;
+  border: 2px solid red;
+  max-width: 900px;
+  display: flex;
+`;
 
 function Tour() {
+  const [pic, setPic] = useState(tourPic);
+  function changeImages() {
+    if (pic == tourPic) {
+      
+    }
+  }
   return (
-    <div className='tour'>
-        <div className="container">
-            <div className="text-area">
-                <h1>Take a tour </h1>
-            </div>
-            <div className="img-area">
-                {/* <img src={tourPic} alt="" style={{height:"40rem"}}/> */}
-            </div>
-        </div>
+    <div className="tour">
+      <Container>
+        <Wrapper>
+          <TextArea>
+            <h1>Take a tour </h1>
+            <List id="1">things rthat we do</List>
+            <List id="2">things rthat we do</List>
+            <List id="3">things rthat we do</List>
+          </TextArea>
+          <ImageArea>
+            {/* <img src={tourPic} alt="" style={{height:"40rem"}}/> */}
+          </ImageArea>
+        </Wrapper>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Tour
+export default Tour;
