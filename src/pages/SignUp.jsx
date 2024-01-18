7e6618c0322d2b1fbea91d86a511046bbeaa4844
Hyelnati D. Yerima <hyelnati@gmail.com>
2024-01-18 14:28:28 +0100

    push

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -24,7 +24,7 @@ function SignUp() {
     if (checkPasswordEmpty && checkPasswordMatch)
       try {
         const response =await axios.post(
-            "http://localhost:8080/api/v1/user/register",
+            "https://blumafrica-production.up.railway.app/api/v1/user/register",
             {username, email, password},
             {
               headers: {
