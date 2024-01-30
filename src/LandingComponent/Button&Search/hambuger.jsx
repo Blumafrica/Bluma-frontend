e81b75e1a15571af7bc7707e521c4481b39c6686
mikofrac <m.ogbechie@native.semicolon.africa>
2024-01-30 18:17:48 +0100

    rebase

--- src/LandingComponent/Button&Search/hambuger.jsx
+++ src/LandingComponent/Button&Search/hambuger.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import "./hambuger.css"
+import React, { useState } from "react";
+import "./hambuger.css";
+import MobileNav from "../../pages/mobileNav";
+import { useNavigate } from "react-router-dom";
+
 function Hambuger() {
+  const [display, setDisplay] = useState(false); 
+  const navigate = useNavigate();
+
+  const displayMobileNav = () => {
+    navigate("/Hambuger")
+  };
+
   return (
-    <div className='hambuger'>
-       <span></span>
-       <span></span>
-       <span></span> 
+    <div className="hambuger" onClick={displayMobileNav}>
+      <span></span>
+      <span></span>
+      <span></span>
+      {display && <MobileNav status={display} />}
     </div>
-  )
+  );
 }
 
-export default Hambuger
+export default Hambuger;
