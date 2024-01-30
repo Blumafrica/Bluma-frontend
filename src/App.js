3f80b165e234c4a7f6ec149c5093f62932b61507
Adio Awwal <126446557+Adio03@users.noreply.github.com>
2024-01-30 17:53:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,34 +8,31 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PostView from "./pages/post/postView";
 import Post from "./pages/post/post";
 import AdminLogin from "./pages/admin/AdminLogin";
-// import Login from "./pages/login";
+import Login from "./pages/login";
 import Error from "./pages/Error";
 import LandingPage from "./pages/landinPage/LandingPage";
-import Profile from "./pages/UserProfile/userProfile";
-
+import Profile from "./pages/UserProfile/Profile";
 import PostArticlePage from "./pages/PostArticlePage";
 // import { Login } from "./pages/login";
-import Login from "./pages/login";
-import SignUp from "./pages/SignUp";
+import MobileNav from "./pages/mobileNav";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/About" element={<About />} />
         <Route path="/Products" element={<Products />} />
         <Route path="/events" element={<Events />} />
-        <Route path="/PostView" element={<PostView/>}/>
-        <Route path="/PostArticle" element={<Post/>}/>
-        <Route path="/AdminLogin" element={<AdminLogin/>}/>
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/Error" element={<Error/>}/>
-        <Route path="/PostArticlePage" element={<PostArticlePage/>}/>
-        <Route path ="/UserProfile" element={<Profile/>}/>
-        <Route path="/SignUp" element={<SignUp/>}/>
-
+        <Route path="/PostView" element={<PostView />} />
+        <Route path="/PostArticle" element={<Post />} />
+        <Route path="/AdminLogin" element={<AdminLogin />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Error" element={<Error />} />
+        <Route path="/PostArticlePage" element={<PostArticlePage />} />
+        <Route path="/UserProfile" element={<Profile />} />
+        <Route path="/Hambuger" element={<MobileNav />} />
       </Routes>
     </Router>
   );
