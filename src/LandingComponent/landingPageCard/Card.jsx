372bf6e271b775318aac2331b045d263faf87a7d
a-mariam <mariiam22222@gmail.com>
2024-04-07 22:51:08 +0100

    still fixing landing page post cards

--- src/LandingComponent/landingPageCard/Card.jsx
+++ src/LandingComponent/landingPageCard/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import "./card.css";
+import CardCss from "./card.css";
 import likeIcon from "../assets/like 1.svg";
 import { useNavigate } from "react-router-dom";
+
 
 function Card(props) {
   const navigate = useNavigate();
@@ -9,19 +10,26 @@ function Card(props) {
     navigate("/PostView");
   }
   return (
-    <div id="card" className={props.class} class="bg-pink-200 " >
+    <div id="card" className={props.class} class=" " >
        <div className="post-info">
           <div style={{ display: "flex" }} class="h-[5rem] w-[30rem] ml-[15px]  mt-[2rem]">
             <div class="grid place-content-center h-[5rem] w-[4rem]  "><img src={props.userPic} alt="" className="user-pic" /></div>
-            <div class="grid gap-0 bg-purple-200 w-[20rem]">
-              <p class="mt-5px " className={`${Card.userName}`}>{props.userName}</p>
-              <p>{props.date}</p>
+            <div className="grid  bg-purple- w-[20rem]">
+              <p class="mt-5px " className="userName">{props.userName}</p>
+              <p className="date">{props.date}</p>
             </div>
           </div>
+       </div>
+        {/*<div id="divider"></div>*/}
+        <div id="imagesCard" class="" >
+          {props.images.length > 0
+            
+          }
+
         </div>
-      <img src={props.coverPic} alt="" className="cover-pic" />
+      {/*<img src={props.coverPic} alt="" className="cover-pic" />*/}
       <div className="card-container">
-        <h2>{props.heading}</h2>
+        <h2 id="divider">{props.heading}</h2>
         <h5>{props.description}</h5>
         <p>{props.summary}</p>
       </div>
