860cc8f1a7031d3bda0d31077ba041efc4d2070b
a-mariam <mariiam22222@gmail.com>
2024-07-03 17:31:54 +0100

    fixing

--- src/LandingComponent/Button&Search/BlumaLogo.jsx
+++ src/LandingComponent/Button&Search/BlumaLogo.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./blumaLogo.css"
+import PropTypes from "prop-types";
 
-function BlumaLogo() {
+
+function BlumaLogo({textColor}) {
   const navigate = useNavigate()
   function returnHome(){
     navigate("/")
   }
   return (
     <div className="bluma-logo">
-      <button onClick={returnHome}>
+      <button
+          style={{color: textColor}}
+          onClick={returnHome}>
         Blu<span>ma</span>
       </button>
     </div>
   );
 }
 
-export default BlumaLogo;
+BlumaLogo.propTypes = {
+    textColor: PropTypes.string.isRequired,
+};
+
+export default BlumaLogo
+;
